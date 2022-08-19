8672a43574e91e9db513c70916c0ff6b3dfa77c2
Marclemer <marc-lemer@hotmail.com>
2022-08-19 10:02:50 +0200

    mise en place de la nav Bar

--- src/containers/nav-bar/nav-bar.jsx
+++ src/containers/nav-bar/nav-bar.jsx
@@ -1,24 +1,28 @@
-import { Drawer,Avatar} from "@mui/material";
+import { Avatar,Divider,Stack} from "@mui/material";
+import  style  from "./nav-bar.module.scss";
 import  IconNav from './icon-nav.jpg';
 import  IconNavSec from './icon-navSec.jpg';
 import  IconNavTier from './nav-BarTier.jpg';
 
 
-const NavBar=({isVisible, onCloseMenu})=>{
+const NavBar=()=>{
 
     return(
-        <Drawer anchor="left" open={isVisible} onClose={onCloseMenu}>
-            lucie
-            <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
-            Ellie
-            <Avatar alt='Logo menu' src={IconNavSec} sx={{width:90,height:90}}/>
-            Nami
-            <Avatar alt='Logo menu' src={IconNavTier} sx={{width:90,height:90}}/>
-            <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
-            <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
-            <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
+        <nav className={style.nav}>
+            <Stack direction='column' divider={<Divider orientation="horizontal"/>} spacing={2}>
+                <Avatar alt='Logo menu' src={IconNav} className={style.logo} />
+                <p>Lucie</p>
+                <Avatar alt='Logo menu' src={IconNavSec} className={style.logo} />
+                <p>Elie</p>
+                <Avatar alt='Logo menu' src={IconNavTier} className={style.logo} />
+                <p>Nami</p>
+                {/* <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
+                <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/>
+                <Avatar alt='Logo menu' src={IconNav} sx={{width:90,height:90}}/> */}
+                <p>ICI MENU</p>
+            </Stack>
+        </nav>
 
-        </Drawer>
     );
 };
 
